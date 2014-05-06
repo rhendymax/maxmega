d0c5eedd7379fdd384bc9a8dc196804313ee4d78
rhendymaxmega@gmail.com <rhendymaxmega@gmail.com@e9acb763-b77c-563a-04dd-e95b501ba791>
2014-05-06 09:54:37 +0000

    Change Wizard Report Oustanding PO.

--- purchase/wizard/param_po_oustanding_report.py
+++ purchase/wizard/param_po_oustanding_report.py
@@ -26,20 +26,59 @@ class param_po_oustanding_report(osv.osv_memory):
     _name = 'param.po.oustanding.report'
     _description = 'Param PO Oustanding Report'
     _columns = {
-        'date_from': fields.date("From Date", required=True),
-        'date_to': fields.date("To Date", required=True),
-        'partner_code_from':fields.many2one('res.partner', 'Supplier Code From', domain=[('supplier','=',True)], required=False),
-        'partner_code_to':fields.many2one('res.partner', 'Supplier Code To', domain=[('supplier','=',True)], required=False),
-        'po_from':fields.many2one('purchase.order', 'PO From', required=False),
-        'po_to':fields.many2one('purchase.order', 'PO To', required=False),
-        # 'product_id': fields.many2one('product.product', 'Item Code', domain=[('sale_ok','=',True)], change_default=True),
+        'supp_selection': fields.selection([('all','Supplier & Sundry'),('supplier', 'Supplier Only'),('sundry','Sundry Only')],'Supplier Selection', required=True),
+        'supplier_search_vals': fields.selection([('code','Supplier Code'),('name', 'Supplier Name')],'Supplier Search Values', required=True),
+        'filter_selection': fields.selection([('all_vall','All'),('def','Default'),('input', 'Input'),('selection','Selection')],'Supp Filter Selection', required=True),
+        'partner_default_from':fields.many2one('res.partner', 'Supplier From', domain=[('supplier','=',True)], required=False),
+        'partner_default_to':fields.many2one('res.partner', 'Supplier To', domain=[('supplier','=',True)], required=False),
+        'partner_input_from': fields.char('Supplier From', size=128),
+        'partner_input_to': fields.char('Supplier To', size=128),
+        'partner_ids' :fields.many2many('res.partner', 'report_oustanding_supplier_rel', 'report_id', 'partner_id', 'Supplier', domain=[('supplier','=',True)]),
+        'date_selection': fields.selection([('none_sel','None'),('date_sel', 'Date')],'Type Selection', required=True),
+        'date_from': fields.date("From Date"),
+        'date_to': fields.date("To Date"),
+        'po_selection': fields.selection([('all_vall','All'),('def','Default'),('input', 'Input'),('selection','Selection')],'PO Filter Selection', required=True),
+        'po_default_from':fields.many2one('purchase.order', 'PO From', domain=[('state','=','approved')], required=False),
+        'po_default_to':fields.many2one('purchase.order', 'PO To', domain=[('state','=','approved')], required=False),
+        'po_input_from': fields.char('PO From', size=128),
+        'po_input_to': fields.char('Po To', size=128),
+        'po_ids' :fields.many2many('purchase.order', 'report_oustanding_po_rel', 'report_id', 'po_id', 'Purchase Order', domain=[('state','=','approved')]),
     }
 
     _defaults = {
-        'date_from': lambda *a: time.strftime('%Y-01-01'),
-        'date_to': lambda *a: time.strftime('%Y-%m-%d')
+#        'date_from': lambda *a: time.strftime('%Y-01-01'),
+#        'date_to': lambda *a: time.strftime('%Y-%m-%d')
+        'date_selection': 'none_sel',
+        'supp_selection': 'all',
+        'supplier_search_vals': 'code',
+        'filter_selection': 'all_vall',
+        'po_selection': 'all_vall',
     }
+    
+    def onchange_supp_selection(self, cr, uid, ids, supp_selection, context=None):
+        if context is None:
+            context = {}
+        
+        res = {'value': {'partner_code_from': False, 'partner_code_to':False, 'partner_ids':False}}
 
+        if supp_selection:
+            if supp_selection == 'all':
+                res['domain'] = {'partner_code_from': [('supplier','=',True)],
+                                 'partner_code_to': [('supplier','=',True)],
+                                 'partner_ids': [('supplier','=',True)],
+                                 }
+            elif supp_selection == 'supplier':
+                res['domain'] = {'partner_code_from': [('supplier','=',True),('sundry', '=', False)],
+                                 'partner_code_to': [('supplier','=',True),('sundry', '=', False)],
+                                 'partner_ids': [('supplier','=',True),('sundry', '=', False)],
+                                 }
+            elif supp_selection == 'sundry':
+                res['domain'] = {'partner_code_from': [('sundry','=',True),('supplier', '=', True)],
+                                 'partner_code_to': [('sundry','=',True),('supplier', '=', True)],
+                                 'partner_ids': [('sundry','=',True),('supplier', '=', True)],
+                                 }
+        return res
+    
     def create_vat(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
@@ -54,29 +93,29 @@ class param_po_oustanding_report(osv.osv_memory):
 
 param_po_oustanding_report()
 
-class purchase_order_line(osv.osv):
-    _inherit = "purchase.order.line"
-    _description = "Purchase Order Line"
-
-    def _qty_oustanding(self, cr, uid, ids, name, arg, context=None):
-        if not ids: return {}
-        res = {}
-        stock_move_obj = self.pool.get("stock.move")
-        product_uom_obj = self.pool.get("product.uom")
-        qty_oustanding = 0.00
-        for obj in self.browse(cr, uid, ids, context=context):
-            qty_delivery = 0
-            move_ids = stock_move_obj.search(cr, uid, [('purchase_line_id','=',obj.id),('state','=','done')])
-            if move_ids:
-                for mv in stock_move_obj.browse(cr, uid, move_ids, context=context):
-                    qty_delivery = qty_delivery + product_uom_obj._compute_qty(cr, uid, mv.product_uom.id, mv.product_qty, mv.product_id.uom_id.id)
-            res[obj.id] = obj.product_qty - qty_delivery
-        return res
-
-    _columns = {
-        'oustanding_qty': fields.function(_qty_oustanding, type='float', string='Total oustanding_qty'),
-    }
-
-purchase_order_line()
+#class purchase_order_line(osv.osv):
+#    _inherit = "purchase.order.line"
+#    _description = "Purchase Order Line"
+#
+#    def _qty_oustanding(self, cr, uid, ids, name, arg, context=None):
+#        if not ids: return {}
+#        res = {}
+#        stock_move_obj = self.pool.get("stock.move")
+#        product_uom_obj = self.pool.get("product.uom")
+#        qty_oustanding = 0.00
+#        for obj in self.browse(cr, uid, ids, context=context):
+#            qty_delivery = 0
+#            move_ids = stock_move_obj.search(cr, uid, [('purchase_line_id','=',obj.id),('state','=','done')])
+#            if move_ids:
+#                for mv in stock_move_obj.browse(cr, uid, move_ids, context=context):
+#                    qty_delivery = qty_delivery + product_uom_obj._compute_qty(cr, uid, mv.product_uom.id, mv.product_qty, mv.product_id.uom_id.id)
+#            res[obj.id] = obj.product_qty - qty_delivery
+#        return res
+#
+#    _columns = {
+#        'oustanding_qty': fields.function(_qty_oustanding, type='float', string='Total oustanding_qty'),
+#    }
+#
+#purchase_order_line()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
